f746126104c16b731c0deab5b6be78989e4e644f
Mert Bildirici <hmertbildirici@gmail.com>
2024-09-17 19:28:13 -0400

    Initial Commit

---
+++ Students Who Sit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Students Who Sit
+//
+//  Created by Mert Bildirici on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Students Who Sit/Students_Who_SitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Students_Who_SitApp.swift
+//  Students Who Sit
+//
+//  Created by Mert Bildirici on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Students_Who_SitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
